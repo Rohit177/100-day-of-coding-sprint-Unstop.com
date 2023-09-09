09eb85ceff2a1f02fd8e27329f49be4e7c410348
Rohit Jadhav <34981708+Rohit177@users.noreply.github.com>
2023-09-09 17:38:27 +0530

    Update Ravi_caught_in_strings_solution.py

--- Day-5/Code 1 Ravi Caught in Strings/Ravi_caught_in_strings_solution.py
+++ Day-5/Code 1 Ravi Caught in Strings/Ravi_caught_in_strings_solution.py
@@ -1,1 +1,30 @@
+def longest_palindromic_substring(s):
+    longest = ""
+    
+    for i in range(len(s)):
+        left, right = i, i
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        odd_palindrome = s[left + 1:right]
+        
+        left, right = i, i + 1
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        even_palindrome = s[left + 1:right]
+        
+        if len(odd_palindrome) > len(longest):
+            longest = odd_palindrome
+        if len(even_palindrome) > len(longest):
+            longest = even_palindrome
+    
+    return len(longest)
+
+n = int(input())
+input_str = input()
+
+result = longest_palindromic_substring(input_str)
+
+print(result)
 
